cb7614f0c10bc61916b2c5b8d1731bfadc8ce4fd
aditarca <74060726+aditarca@users.noreply.github.com>
2024-05-16 23:10:09 -0400

    Update validate.py

--- docker/validate.py
+++ docker/validate.py
@@ -13,7 +13,7 @@ import numpy as np
 COLS = {
     "1": {
         'ID': str,
-        'GA': np.float64
+        'GA_prediction': np.float64
     },
     "2": {
         'ID': str,
@@ -91,7 +91,7 @@ def validate(gold_file, pred_file, task_number):
     errors = []
 
     gold = pd.read_csv(gold_file,
-                       index_col="Sample")
+                       index_col=ID")
     try:
         pred = pd.read_csv(pred_file,
                            usecols=COLS[task_number],
